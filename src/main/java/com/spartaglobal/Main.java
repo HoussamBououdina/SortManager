53f72eeebd3495a0d21493a16743dad8b6a6d97c
Houssam Eddine Bououdina <houssamboudina@gmail.com>
2022-09-15 10:55:10 +0100

    added MVC, still need to add the view for displaying arrays, also added random rarray generation and lets the user decide the array size

--- src/main/java/com/spartaglobal/Main.java
+++ src/main/java/com/spartaglobal/Main.java
@@ -2,31 +2,7 @@ package com.spartaglobal;
 
 public class Main {
     public static void main(String[] args) {
-        BubbleSort b= new BubbleSort();
-       BinarySort b1 = new BinarySort();
-        MergeSort m = new MergeSort();
-        int[] arr= {5,2,7,5,5,5,5,2,2,2,10,-1,-2};
-        int[] arr2 =b.sortArray(arr);
-        int[] arr3 =b1.sortArray(arr);
-        int[] arr4 =m.sortArray(arr);
-
-        System.out.println("Array sorted in Ascending order");
-        for (int j : arr2) {
-
-            System.out.print(j+" ");
-        }
-        System.out.println();
-        System.out.println("Array sorted in binary sort");
-       for (int j : arr3) {
-           System.out.print(j+" ");
-       }
-
-        System.out.println();
-        System.out.println("Array sorted in merge sort");
-        for (int j : arr4) {
-
-            System.out.print(j+" ");
-        }
-
+        Starter.start();
     }
+
 }
